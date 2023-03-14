86e0e8b7f553b6593f5b547af365f8a0d091a68f
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2023-03-15 05:40:49 +0700

    Create

---
+++ _ss/top.js
@@ -0,0 +1,15 @@
+function minCSS(css) {
+  css = css.replace(/\/\*[\s\S]*?\*\/|\/\/.*?$/gm, '');
+  css = css.replace(/\s+/g, ' ').replace(/\s*([\{\}\:\;\,])\s*/g, '$1');
+  css = css.replace(/\{\s*/g, '{');
+  css = css.replace(/\}\s*/g, '}');
+  css = css.replace(/\:\s*/g, ':');
+  css = css.replace(/\,\s*/g, ',');
+  return css;
+}
+function minJS(js) {
+  js = js.replace(/\/\*[\s\S]*?\*\/|\/\/.*?$/gm, '');
+  js = js.replace(/\s+/g, ' ');
+  js = js.replace(/\s*([\=\+\-\*\/\%\&\|\^\!\~\?\<\>\,\;\:\{\}\(\)\[\]])\s*/g, '$1');
+  return js;
+}
