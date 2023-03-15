4d4988968303485e00939aaa3b5d8a817206b7cc
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2023-03-15 12:21:08 +0700

    Update top2.js

--- _ss/top2.js
+++ _ss/top2.js
@@ -7,10 +7,10 @@ function minCSS(css) {
   css = css.replace(/\,\s*/g, ',');
   return css;
 }
-function minJS(js) {
-  js = js.replace(/\/\*[\s\S]*?\*\/|\/\/.*?$/gm, '');
-  js = js.replace(/\s+/g, ' ');
-  js = js.replace(/\s*([\=\+\-\*\/\%\&\|\^\!\~\?\<\>\,\;\:\{\}\(\)\[\]])\s*/g, '$1');
-  js = js.replace(/(?<=\${[^}]+}) /g, '');
-  return js;
+function minJS(code) {
+  let minifiedCode = code.replace(/(?<=\s)(`|\$\{)/g, " $1"); // tambahkan spasi sebelum template literal atau variable yang memiliki spasi sebelumnya
+  minifiedCode = minifiedCode.replace(/\s+/g, " "); // hapus spasi berlebih
+  minifiedCode = minifiedCode.replace(/(\r\n|\n|\r)/gm, ""); // hapus karakter baris baru
+  minifiedCode = minifiedCode.replace(/(\/\*([\s\S]*?)\*\/)|(\/\/(.*)$)/gm, ""); // hapus komentar
+  return minifiedCode.trim(); // hapus spasi di awal dan akhir
 }
